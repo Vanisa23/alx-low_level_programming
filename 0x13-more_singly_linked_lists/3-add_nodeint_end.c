622337f01cfb0d8e3188f15fe03ea25cb89ecc56
Vanisa <iamyirmeyahu@gmail.com>
2023-04-04 19:48:20 -0700

    C - More singly linked lists

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -9,25 +9,26 @@
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
- listint_t *new;
- listint_t *temp = *head;
+	listint_t *new;
+	listint_t *temp = *head;
 
- new = malloc(sizeof(listint_t));
- if (!new)
- return (NULL);
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
 
- new->n = n;
- new->next = NULL;
- if (*head == NULL)
- {
- *head = new;
- return (new);
- }
+	new->n = n;
+	new->next = NULL;
 
- while (temp->next)
- temp = temp->next;
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
 
- temp->next = new;
+	while (temp->next)
+		temp = temp->next;
 
- return (new);
+	temp->next = new;
+
+	return (new);
 }
