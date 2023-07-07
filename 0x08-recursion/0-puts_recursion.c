932c3e75834ddd490140e8d0dc00a5c7d055057a
Vanisa23 <iamyirmeyahu@gmail.com>
2023-07-07 10:03:59 -0700

    correct the errors in 1

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,17 @@
+#include "main.h"
 /**
  * _puts_recursion - function like puts();
  * @s: input
  * Return: Always 0 (Success)
  */
-#include "main.h"
 
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		putchar('\n');
+		_putchar('\n');
 		return;
 	}
-	putchar(*s);
+	_putchar(*s);
 	_puts_recursion(s + 1);
 }
