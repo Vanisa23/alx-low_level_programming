16040add4978dfcef8fd646ef151b26e2502fc1d
Vanisa23 <iamyirmeyahu@gmail.com>
2023-07-28 03:03:43 -0700

    add _putchar.c in the main.h file

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,18 +1,19 @@
 #include "main.h"
+
 /**
  * _puts_recursion - functions like puts()
  * @s: string input.
  *
  * Return: Always 0 (Success)
  */
-
 void _puts_recursion(char *s)
 {
-	if (*s == 0)
+	if (*s)
 	{
-		_putchar('\n');
-		return;
-	}
 		_putchar(*s);
 		_puts_recursion(s + 1);
+	}
+
+	else
+		_putchar('\n');
 }
